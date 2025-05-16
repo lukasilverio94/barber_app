1125a5c17994313b54755e1e5a825daf2ed88bb9
lukasilverio <lukasilverio94@gmail.com>
2025-05-16 09:44:57 +0200

    (db): add migrations with flyway

--- src/main/resources/db/migration/V1__database-init.sql
+++ src/main/resources/db/migration/V1__database-init.sql
@@ -1,10 +1,10 @@
--- Primeiro você cria um tipo que vai ser usado depois na craição
--- da tablea de usuário
+-- ENUMS
 CREATE TYPE user_type AS enum ('BARBER', 'CUSTOMER');
+CREATE TYPE appointment_status AS enum ('REQUESTED', 'CONFIRMED', 'DENIED', 'CANCELED');
+CREATE TYPE service_type AS enum ('HAIRCUT', 'BEARD', 'HAIRCUT_AND_BEARD');
 
--- Daí aqui você cria a tabela de usuário e tem um campo que definiria o tipo do usuário
--- Dessa forma tu não precisa ter duas tabelas com praticamente os mesmos dados pra os
--- dois tipos de usuário
+-- TABLES
+
 CREATE TABLE app_user
 (
     id        uuid,
@@ -15,30 +15,22 @@ CREATE TABLE app_user
     CONSTRAINT pk_app_user_id PRIMARY KEY (id)
 );
 
--- Isso aqui fiz depois da proxima tabela. Criei o novo tipo e
--- depois só fiz um update na tabela timeslot
-CREATE TYPE timeslot_availability AS enum ('AVAILABLE', 'UNAVAILABLE');
-
 -- Separadamente tu cria uma tabela com os horários disponíveis
 -- pra um barbeador, ai nela tu referencia o id do barbeador que tá com aquela vaga
 -- Nessa tabla tu só colocaria os que tão disponpiveis. Assim que não tiver mais disponivel tu precisa remover
 -- ou criar um outro status tipo "OCUPADO"
 CREATE TABLE timeslot
 (
-    id                    uuid                  NOT NULL,
-    day                   date                  NOT NULL,
-    start_time            time                  NOT NULL,
-    end_time              time                  NOT NULL,
-    barber_id             uuid                  NOT NULL,
-    timeslot_availability timeslot_availability NOT NULL DEFAULT 'AVAILABLE',
+    id                    uuid                                                                       NOT NULL,
+    day                   date                                                                       NOT NULL,
+    start_time            time                                                                       NOT NULL,
+    end_time              time                                                                       NOT NULL,
+    barber_id             uuid                                                                       NOT NULL,
+    timeslot_availability varchar(20) check ( timeslot_availability in ('AVAILABLE', 'UNAVAILABLE')) NOT NULL DEFAULT 'AVAILABLE',
     CONSTRAINT pk_timeslot_id PRIMARY KEY (id),
     CONSTRAINT fk_timeslot_barber FOREIGN KEY (barber_id) REFERENCES app_user (id),
     CONSTRAINT unique_timeslot UNIQUE (day, start_time, end_time, barber_id)
 );
-
--- Aqui tu cria o tipo de status do agendamento
-CREATE TYPE appointment_status AS enum ('REQUESTED', 'CONFIRMED', 'DENIED', 'CANCELED');
-CREATE TYPE service_type AS enum ('HAIRCUT', 'BEARD', 'HAIRCUT_AND_BEARD');
 
 -- Esse aqui seria a tabela com as consultas que já estão marcadas ou então
 -- esperando o barbeiro confirmar ou negar e tal
