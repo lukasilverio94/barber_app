7cb526575b6c44aa9a97917ef1f9226919728177
lukasilverio <lukasilverio94@gmail.com>
2025-05-25 00:09:38 +0200

    implements user details service

---
+++ src/main/java/com/barbershop/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.barbershop.security;
+
+import com.barbershop.repository.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AppUserRepository userRepository;
+
+    public UserDetailsServiceImpl(AppUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email " + username));
+    }
+}
