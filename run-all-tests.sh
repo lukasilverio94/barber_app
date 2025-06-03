795b7b5a5a9a2a8133c83915ac3c20df2dc594a9
lukasilverio <lukasilverio94@gmail.com>
2025-06-03 22:55:52 +0200

    add run all tests script

---
+++ run-all-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Running all tests using Maven..."
+mvn clean test
--- run-test-profile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./mvnw spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=test"
