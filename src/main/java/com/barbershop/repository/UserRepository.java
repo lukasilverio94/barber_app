f204b27c7793f9e9d40f3f0e351dd8568d6f623e
lukasilverio <lukasilverio94@gmail.com>
2025-06-03 16:13:47 +0200

    delete unused file

--- src/main/java/com/barbershop/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.barbershop.repository;
-
-import com.barbershop.model.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<AppUser, UUID> {
-}
