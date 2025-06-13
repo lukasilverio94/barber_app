8674a19d7f0a47fc56318627406b1631c2113f51
lukasilverio <lukasilverio94@gmail.com>
2025-06-13 15:46:29 +0200

    add args and no args constructors to customer entity

--- src/main/java/com/barbershop/model/Customer.java
+++ src/main/java/com/barbershop/model/Customer.java
@@ -2,10 +2,12 @@ package com.barbershop.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.List;
+import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -17,4 +19,12 @@ public class Customer extends AppUser {
     @JsonIgnore
     private List<Appointment> appointments;
 
+    public Customer(UUID id, String name, String phone, String email, String password, List<Appointment> appointments) {
+        super(id, name, phone, email, password);
+        this.appointments = appointments;
+    }
+
+    public Customer() {
+
+    }
 }
