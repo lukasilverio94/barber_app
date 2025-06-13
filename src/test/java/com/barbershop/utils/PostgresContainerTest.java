43ea496a9aae1875e4c78017b76c53efc39d8fa9
lukasilverio <lukasilverio94@gmail.com>
2025-06-13 15:45:11 +0200

    (fix) config for PostgresContainerTest update

--- src/test/java/com/barbershop/utils/PostgresContainerTest.java
+++ src/test/java/com/barbershop/utils/PostgresContainerTest.java
@@ -1,28 +1,28 @@
-package com.barbershop.integration;
+package com.barbershop.utils;
 
 import com.barbershop.constants.DockerImageConstants;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.ResourceLock;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ResourceLock("database")
+@ExtendWith(SpringExtension.class)
 @Testcontainers
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public abstract class AbstractPostgresContainerTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class PostgresContainerTest {
 
-    @Container
-    protected static final PostgreSQLContainer<?> postgres =
+     static PostgreSQLContainer<?> postgres =
             new PostgreSQLContainer<>(DockerImageName.parse(DockerImageConstants.POSTGRES_IMAGE));
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
+        postgres.start();
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
